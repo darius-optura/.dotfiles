6cb87feba09434bc3bd876af4bd8a38916d4d7cb
Cupsa Darius <cupsadarius@gmail.com>
2022-04-26 20:09:24 +0300

    add autocomplete functionality

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,6 +31,10 @@ return require('packer').startup(function()
   use 'ThePrimeagen/harpoon'
   use 'kdheepak/lazygit.nvim'
   use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin  
   -- use 'cupsadarius/vim-horizon'
   -- use 'morhetz/gruvbox'
   -- use 'drewtempelmeyer/palenight.vim'
