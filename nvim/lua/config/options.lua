861a4e0cc26a42630022f8c393c8e95fa5609762
Cupsa Darius <cupsadarius@gmail.com>
2024-01-04 17:07:03 +0200

    lazyvim trial

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,6 @@
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -16,12 +13,12 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
--- vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -29,7 +26,7 @@ vim.o.breakindent = true
 -- Save undo history
 vim.o.undofile = true
 vim.o.swapfile = false
-vim.o.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.o.backup = false
 
 -- Case insensitive searching UNLESS /C or capital in search
@@ -37,16 +34,15 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn = "yes"
 
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
--- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
 vim.o.incsearch = true
@@ -56,5 +52,5 @@ vim.o.scrolloff = 8
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.numberwidth = 4
-vim.o.colorcolumn = '80'
+vim.o.colorcolumn = "80"
 vim.o.showtabline = 2
---
+++ nvim/lua/config/refactoring.lua
@@ -0,0 +1,11 @@
+local refactoring = require("refactoring")
+
+refactoring.setup({})
+
+-- remap to open the Telescope refactoring menu in visual mode
+vim.keymap.set(
+  "v",
+  "<leader>rr",
+  "<Esc>:lua require('telescope').extensions.refactoring.refactors()<CR>",
+  { noremap = true }
+)
