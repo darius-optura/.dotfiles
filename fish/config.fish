e89d76a37fb6ddb229c867958a69500c821095dc
Cupsa Darius <cupsadarius@gmail.com>
2024-11-01 18:04:27 +0200

    cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -47,7 +47,7 @@ export PATH="$PATH:$HOME/.rvm/bin"
 export PATH="$PATH:/usr/sbin"
 export PATH="$PATH:/sbin"
 
-export BAT_THEME="Catppuccin"
+export BAT_THEME="Rose Pine Moon"
 export LG_CONFIG_FILE="$HOME/.config/lazygit/config.yml"
 export K9SCONFIG="$HOME/.config/k9s"
 export K9S_CONFIG_PATH="$HOME/.config/k9s"
@@ -56,6 +56,7 @@ export KUBECONFIG="./kubeconfig.yml"
 
 starship init fish | source
 zoxide init fish | source
+fish_config theme choose "Rose Pine Moon"
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '$HOME/.gcloud/path.fish.inc' ]; . '$HOME/.gcloud/path.fish.inc'; end
 
